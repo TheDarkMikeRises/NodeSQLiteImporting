0b710eda22bcc6abcd8e22b0be3b353a850a29a6
JoelTParko <joel.tp@hotmail.co.uk>
2019-07-12 12:43:40 +0100

    It works for real now

--- csvImport.js
+++ csvImport.js
@@ -4,15 +4,16 @@ var multer = require('multer');
 const fs = require('fs');
 const csv = require('csv-parser');
 const sqlite = require('sqlite3');
+const readline = require('readline');
 const results = [];
 
 console.log("hi");
 
 function run(filePath) {
-    let db = new sqlite.Database(filePath);
+    let db = new sqlite.Database('test.db');
     let sql = 'INSERT INTO Control(Name, Category, Reference, Date_Created, Date_Reviewed) VALUES ((?),(?),(?),(?),(?))';
     console.log("Hi");
-    fs.createReadStream('test.csv')
+    fs.createReadStream(filePath)
         .pipe(csv())
         .on('data', (data) => results.push(data))
         .on('end', () => {
@@ -26,5 +27,11 @@ function run(filePath) {
         });
 }
 
-console.log("Enter file path");
-run(console.readline());
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+r1.question('Enter the filepath of your csv file:\n', (answer) => {
+    run(answer);
+    r1.close();
+});
