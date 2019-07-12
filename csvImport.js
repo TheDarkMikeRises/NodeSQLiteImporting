1ced9c666ee176674376e77545972959896a0293
JoelTParko <joel.tp@hotmail.co.uk>
2019-07-12 12:07:38 +0100

    csv importing works

--- csvImport.js
+++ csvImport.js
@@ -1,30 +1,30 @@
 const fs = require('fs');
 const csv = require('csv-parser');
+const sqlite = require('sqlite3');
 const results = [];
 
 console.log("hi");
 
 function run() {
+    let db = new sqlite.Database('./test.db');
+    let sql = 'INSERT INTO Control(Name, Category, Reference, Date_Created, Date_Reviewed) VALUES ((?),(?))';
     console.log("Hi");
     fs.createReadStream('test.csv')
         .pipe(csv())
         .on('data', (data) => results.push(data))
         .on('end', () => {
-            console.log(results);
+            for (const record of results) {
+                db.run(sql, Object.values(record), function(err){
+                    if(err) return console.error(err.message);
+                    console.log(`Rows inserted ${this.changes}`);
+                })
+                console.log(record);
+            }
         });
 }
 
-function success(csvData){
-    csvParser(csvData, {
-        delimiter: ','
-      }, function(err, data) {
-          console.log("HIYA");
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(data);
-        }
-      });
+function insertData(data){ 
+    
 }
 
 run();
