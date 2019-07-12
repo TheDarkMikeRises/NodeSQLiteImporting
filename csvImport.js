a3f5f9f3400c5ae19e213fbc1615bdb710c3c14b
JoelTParko <joel.tp@hotmail.co.uk>
2019-07-12 11:17:29 +0100

    csv import works

--- csvImport.js
+++ csvImport.js
@@ -1,28 +1,30 @@
 const fs = require('fs');
-const csvParser = require('csv-parser');
+const csv = require('csv-parser');
+const results = [];
 
 console.log("hi");
 
 function run() {
     console.log("Hi");
-    fs.readFile('fujControls.csv', {
-        encoding: 'utf-8'
-      }, function(err, csvData) {
+    fs.createReadStream('test.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            console.log(results);
+        });
+}
+
+function success(csvData){
+    csvParser(csvData, {
+        delimiter: ','
+      }, function(err, data) {
+          console.log("HIYA");
         if (err) {
           console.log(err);
+        } else {
+          console.log(data);
         }
-        console.log(csvData);
-        csvParser(csvData, {
-          delimiter: ','
-        }, function(err, data) {
-            console.log("HIYA");
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(data);
-          }
-        });
-    });
+      });
 }
 
 run();
